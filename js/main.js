cf1099fbd0dfe32709a51cc8444701318de32fff
sultanskiyy <sultanov_asadbek@icloud.com>
2025-11-26 17:36:47 +0500

    tugadi

--- js/main.js
+++ js/main.js
@@ -1,25 +1,57 @@
 let teacherCards = document.querySelector(".teacher-cards");
-let addTeacherBtn = document.getElementById("add-teacher-btn");
-let submitTeacher = document.getElementById("submit-teacher");
 let form = document.getElementById("form");
+let outerModal = document.getElementById("outer-modal");
+let addTeacherBtn = document.getElementById("add-teacher-btn");
+let selected = null;
+let paganation = document.getElementById("paganation");
+let page = 1;
 
-addTeacherBtn.addEventListener("click", () => {
-    submitTeacher.classList.remove("hidden");
-});
+addTeacherBtn.addEventListener("click", function () {
+    outerModal.classList.remove("hidden");
+    for (let el of form) {
+        el.value = "";
+        el.checked = false;
+    }
+})
 
-submitTeacher.addEventListener("click", () => {
-    submitTeacher.classList.add("hidden");
-});
+outerModal.addEventListener("click", function () {
+    outerModal.classList.add("hidden")
+    selected = null;
+})
 
-form.addEventListener("click", e => e.stopPropagation());
+form.addEventListener("click", function (e) {
+    e.stopPropagation()
+})
 
-async function getData() {
+async function getData(content , page) {
     try {
-        teacherCards.innerHTML = "";
-        let res = await axios.get("https://692584e782b59600d723f9b8.mockapi.io/teachers");
 
-        res.data.forEach(el => {
-            teacherCards.innerHTML += `
+        let res = await axios.get(
+            `https://692584e782b59600d723f9b8.mockapi.io/teachers?page=${page}&limit=10`
+        );
+        let allRes = await axios.get(
+            "https://692584e782b59600d723f9b8.mockapi.io/teachers"
+        );
+        let pages = Math.ceil(allRes.data.length / 10);
+
+        paganation.innerHTML = ""
+
+        paganation.innerHTML +=
+            `
+        <li onClick="changePage(${page - 1})" class="${page === 1 ? "hidden" : ""} flex items-center cursor-pointer justify-center text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading font-medium rounded-s-base text-sm px-3 h-9 focus:outline-none">Previous</li>
+        `
+
+        for(let i =1; i <= pages; i++){
+            paganation.innerHTML += `<li onClick="changePage(${i})" class="flex cursor-pointer items-center justify-center text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading font-medium text-sm w-9 h-9 focus:outline-none">${i}</li>`
+        }
+
+        paganation.innerHTML += `<li onClick="changePage(${page + 1})"
+                 class="${page === pages ? "hidden" : ""} flex items-center justify-center cursor-pointer text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading font-medium rounded-e-base text-sm px-3 h-9 focus:outline-none">Next
+        </li>`
+
+        content.innerHTML = ""
+        res.data.map((el) => {
+            content.innerHTML += `
             <div class="teacher-card max-w-[300px] w-full" data-id="${el.id}">
                 <div class="group flex flex-col items-center gap-[5px] pt-[30px] bg-white dark:bg-gray-800 rounded-[15px] shadow-md p-4">
 
@@ -63,10 +95,10 @@ async function getData() {
                     </div>
 
                     <div class="mt-4 flex gap-[10px] opacity-0 group-hover:opacity-100 transition-all duration-300 justify-center w-full">
-                        <button class="flex px-[15px] border-zinc-900 rounded-lg items-center bg-zinc-800 hover:bg-zinc-900 py-[3px] border-[1px] gap-[5px] edit-btn">
+                        <button onClick="editTeacher(${el.id})" class="flex px-[15px] border-zinc-900 rounded-lg items-center bg-zinc-800 hover:bg-zinc-900 py-[3px] border-[1px] gap-[5px] edit-btn">
                             <img class="object-cover p-[4px]" src="../assets/image/pencil (1).png" alt="">Edit
                         </button>
-                        <button class="flex px-[15px] text-red-500 border-zinc-900 rounded-lg bg-zinc-800 hover:bg-red-950 items-center py-[3px] border-[1px] gap-[5px] delete-btn">
+                        <button onClick="deleteTeacher(${el.id})" class="flex px-[15px] text-red-500 border-zinc-900 rounded-lg bg-zinc-800 hover:bg-red-950 items-center py-[3px] border-[1px] gap-[5px] delete-btn">
                             <img class="object-cover p-[4px]" src="../assets/image/trash (1).png" alt="">Delete
                         </button>
                     </div>
@@ -80,52 +112,84 @@ async function getData() {
     }
 }
 
-async function addTeacher(teacherObj) {
+getData(teacherCards , page);
+
+function changePage(i){
+    getData(teacherCards , i)
+}
+
+
+async function editTeacher(id) {
+    outerModal.classList.remove("hidden");
+    selected = id;
     try {
-        await axios.post("https://692584e782b59600d723f9b8.mockapi.io/teachers", teacherObj);
-        submitTeacher.classList.add("hidden");
-        await getData();
+        let res = await axios.get(`https://692584e782b59600d723f9b8.mockapi.io/teachers/${id}`);
+        console.log(res.data);
+
+        form[0].value = res.data.name
+        form[1].value = res.data.age
+        form[2].value = res.data.avatar
+        form[3].value = res.data.email
+        form[4].value = res.data.experience
+        form[5].value = res.data.profession
+        form[6].value = res.data.linkedin
+        form[7].value = res.data.phone
+        form[8].value = res.data.raiting
+        form[9].value = res.data.telegram
+        form[10].checked = res.data.gender
     } catch (err) {
         console.log(err);
     }
 }
 
-teacherCards.addEventListener("click", async e => {
-    const deleteBtn = e.target.closest(".delete-btn");
-    if (!deleteBtn) return;
 
-    e.preventDefault();
-
-    const card = deleteBtn.closest(".teacher-card");
-    const id = card.dataset.id;
-
+async function addTeacher(teacherObj) {
     try {
-        await axios.delete(`https://692584e782b59600d723f9b8.mockapi.io/teachers/${id}`);
-        card.remove();
+
+        if (selected) {
+            await axios.put(`https://692584e782b59600d723f9b8.mockapi.io/teachers/${selected}`, teacherObj, outerModal.classList.add("hidden"));
+        } else {
+            await axios.post("https://692584e782b59600d723f9b8.mockapi.io/teachers", teacherObj, outerModal.classList.add("hidden"));
+        }
+        outerModal.classList.add("hidden");
+        selected = null;
+        getData(teacherCards , page)
     } catch (err) {
         console.log(err);
     }
-});
+}
 
-form.addEventListener("submit", e => {
+
+form.addEventListener("submit", function (e) {
     e.preventDefault();
+    const teacherObj = {}
+    teacherObj.name = form[0].value;
+    teacherObj.age = form[1].value;
+    teacherObj.avatar = form[2].value;
+    teacherObj.email = form[3].value;
+    teacherObj.experience = form[4].value;
+    teacherObj.profession = form[5].value;
+    teacherObj.linkedin = form[6].value;
+    teacherObj.phone = form[7].value;
+    teacherObj.raiting = form[8].value;
+    teacherObj.telegram = form[9].value
+    teacherObj.isMarried = form[10].checked;
 
-    const teacherObj = {
-        name: form[0].value,
-        age: form[1].value,
-        avatar: form[2].value,
-        email: form[3].value,
-        experience: form[4].value,
-        profession: form[5].value,
-        linkedin: form[6].value,
-        phone: form[7].value,
-        raiting: form[8].value,
-        telegram: form[9].value,
-        isMarried: form[10].checked
-    };
-
+    console.log(teacherObj);
     addTeacher(teacherObj);
-    form.reset();
-});
+    selected = null;
+})
 
-getData();
+
+async function deleteTeacher(id) {
+    try {
+        await axios.delete(`https://692584e782b59600d723f9b8.mockapi.io/teachers/${id}`);
+        getData(teacherCards , page)
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+
+
+
